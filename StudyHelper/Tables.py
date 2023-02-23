f37962b292c95eaa5820f2c1e69fe1005e9059e1
SamuelDGeorge <38861199+SamuelDGeorge@users.noreply.github.com>
2023-02-23 15:22:48 -0500

    Update Question 2 Survey

---
+++ StudyHelper/Tables.py
@@ -0,0 +1,19 @@
+from IPython.display import HTML 
+
+def Q3Answer():
+    return HTML(filename='./StudyHelper/HTML/ChatGPTIncrease.html')
+
+def Q4Answer():
+    return HTML(filename='./StudyHelper/HTML/CanonicalFeedback.html')
+
+def Q5Answer():
+    return HTML(filename='./StudyHelper/HTML/NCFeedback.html')
+
+def Q6Answer():
+    return HTML(filename='./StudyHelper/HTML/SolFeedback.html')
+
+def Q7Answer():
+    return HTML(filename='./StudyHelper/HTML/SolutionDistribution.html')
+
+def Q8Answer():
+    return HTML(filename='./StudyHelper/HTML/InstructorFeedback.html')
