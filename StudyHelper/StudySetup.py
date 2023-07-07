bc37d9149aad14bb8244b969eb5808841aa73127
SamuelDGeorge <38861199+SamuelDGeorge@users.noreply.github.com>
2023-07-07 14:47:33 -0400

    Lock Notebook Fields and Adjust Autosave

--- StudyHelper/StudySetup.py
+++ StudyHelper/StudySetup.py
@@ -1,6 +1,10 @@
 import StudyHelper.OnlineLogger as OnlineLogger
 
+
 def StudySetup(unique_id, course_taken, notebook_name, ident):
-    base_id = unique_id + '-' + course_taken + '-'
+    if unique_id == "FakeName123":
+        print("Please edit the 'chosen_id' above to a new fake name.")
+        return
+    base_id = unique_id + "-" + course_taken + "-"
     OnlineLogger.start(notebook_name, ident, base_id)
     print("Ready to begin study!")
